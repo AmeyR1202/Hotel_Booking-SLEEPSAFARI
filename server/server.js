2273332ad925b5ba437d2b83c7570dcb85455e24
AmeyR1202 <ameyrakhe01@gmail.com>
2025-06-29 10:31:18 +0530

    Added Clerk Auth in Backend

--- server/server.js
+++ server/server.js
@@ -2,11 +2,20 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
 
 const APP = express();
 APP.use(cors());
+
+//Middlewares
+APP.use(express.json());
+APP.use(clerkMiddleware());
+
+// API TO LISTEN CLERK WEBHOOKS
+APP.use("/api/clerk", clerkWebhooks);
 
 APP.get("/", (req, res) => res.send("API IS WORKING"));
 
