bb80196cd4f6b86d25d26abff9a2846f5e0ee38c
AmeyR1202 <ameyrakhe01@gmail.com>
2025-07-02 16:28:47 +0530

    Added APIs for Fetching the Hotel Data

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,5 +1,15 @@
 import multer from "multer";
+import path from "path";
 
-const upload = multer({ storage: multer.diskStorage() });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // or wherever you want to save files
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
 
 export default upload;
--- server/models/Hotel.js
+++ server/models/Hotel.js
@@ -11,6 +11,7 @@ const hotelSchema = new mongoose.Schema(
       required: true,
     },
     city: { type: String, required: true },
+    image: { type: String },
   },
   { timestamps: true }
 );
